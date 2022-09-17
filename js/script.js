4ff809372b2cb9b232c5c0f0fa6193a80b3e8ce9
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-09-17 09:09:42 +0200

    add navbar scroll effect

--- js/script.js
+++ js/script.js
@@ -19,3 +19,27 @@ $(".blog-carousel").owlCarousel({
         }
     }
 });
+
+let nav=document.getElementsByClassName("navbar")[0]
+let activeLink=document.querySelectorAll('.navbar .nav-link.active')[0]
+let ul=document.querySelector(".navbar-nav")
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+window.addEventListener('scroll',(e)=>{
+    if(window.pageYOffset >= vh){      
+        nav.classList.remove('position-absolute','text-white');
+        ul.classList.remove('text-links')
+        nav.classList.add("fixedNav","shadow-sm");
+        activeLink.classList.remove("activeLinkWhite");
+        activeLink.classList.add('activeLinkBlack');
+    }
+    else{
+        nav.classList.remove("fixedNav","shadow-sm");
+        ul.classList.add('text-links')
+        nav.classList.add('position-absolute','text-white');
+        activeLink.classList.remove("activeLinkBlack");
+        activeLink.classList.add('activeLinkWhite');
+
+    }
+})
+
